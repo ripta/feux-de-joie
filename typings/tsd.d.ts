0e1fafc0369fae1630aaabf1de74d2446594accb
Ripta Pasay <github@r8y.org>
2015-05-30 23:19:02 -0700

    tsd: +body-parser, +errorhandler

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="express/express.d.ts" />
 /// <reference path="angular2/angular2.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="errorhandler/errorhandler.d.ts" />
