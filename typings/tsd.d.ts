0f65e6de5172519e3d0072543797e33748d45339
Ripta Pasay <github@r8y.org>
2015-05-31 00:52:25 -0700

    tsd: +socket.io

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="angular2/angular2.d.ts" />
 /// <reference path="body-parser/body-parser.d.ts" />
 /// <reference path="errorhandler/errorhandler.d.ts" />
+/// <reference path="socket.io/socket.io.d.ts" />
