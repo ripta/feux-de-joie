39e5a126d464c06902799efa7a1c82346807be5c
Ripta Pasay <github@r8y.org>
2015-05-31 01:43:08 -0700

    routes: remove unnecessary routes for now

--- routes/chatrooms.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-
-import express = require("express")
-
-export function index(req: express.Request, rsp: express.Response) {
-	var locals = { }
-	rsp.write(JSON.stringify(locals))
-	rsp.end
-}
