0ba2c3a1dea95d65c722c9f5d947fd66a432d6d9
Ripta Pasay <github@r8y.org>
2015-06-01 02:09:57 -0700

    chat-box: connect with message service

--- public/chat/chat-box.ts
+++ public/chat/chat-box.ts
@@ -1,18 +1,27 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-import {Component, View} from "angular2/angular2"
+import {NgFor, Component, View} from "angular2/angular2"
+import {MessageService} from "./message-service"
 
 @Component({selector: "chat-box"})
-@View({templateUrl: "components/chat-box.html"})
+@View({templateUrl: "components/chat-box.html", directives: [NgFor]})
 export class ChatBoxComponent {
-  constructor() { }
+  messageService: MessageService
+  nickname: string
+
+  constructor() {
+    this.messageService = new MessageService()
+    this.nickname = "person" + Math.round(Math.random() * 100000)
+    this.sendMessage("Hello!")
+  }
 
   processMessageFrom = ($event, messageBox) => {
     this.sendMessageFrom(messageBox)
   }
 
   sendMessage = (message) => {
-    console.log(message)
+    this.messageService.add(this.nickname, message, new Date())
+    // console.log(this.messages)
   }
 
   sendMessageFrom = (messageBox) => {
